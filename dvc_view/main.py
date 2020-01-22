780deb00c9388a2bc57435e9e892c87f7494e1a4
Jonas <jonas.annuscheit@htw-berlin.de>
2020-01-22 15:42:45 +0100

    fix subprocess.call

--- dvc_view/main.py
+++ dvc_view/main.py
@@ -11,21 +11,21 @@ def create_fix_demo_stages(path):
     os.chdir(path)
     gitrepo = git.Repo.init()
     dvcrepo = dvc.repo.Repo.init()
-    subprocess.call(['dvc', 'run', '-o', 'out1_1.txt', '"echo text>out1_1.txt"'])
-    subprocess.call(['dvc', 'run', '-d', 'out1_1.txt', '-o', 'out1_2.txt', '"echo text>out1_2.txt"'])
-    subprocess.call(['dvc', 'run', '--no-exec', '-d', 'out1_2.txt', '-o', 'out1_3.txt', '"echo text>out1_3.txt"'])
+    subprocess.call(['dvc', 'run', '-o', 'out1_1.txt', 'echo text>out1_1.txt'])
+    subprocess.call(['dvc', 'run', '-d', 'out1_1.txt', '-o', 'out1_2.txt', 'echo text>out1_2.txt'])
+    subprocess.call(['dvc', 'run', '--no-exec', '-d', 'out1_2.txt', '-o', 'out1_3.txt', 'echo text>out1_3.txt'])
 
-    subprocess.call(['dvc', 'run', '-o', 'out2_1.txt', '"echo text>out2_1.txt"'])
-    subprocess.call(['dvc', 'run', '-d', 'out2_1.txt', '-o', 'out2_2.txt', '"echo text>out2_2.txt"'])
-    subprocess.call(['dvc', 'run', '-d', 'out2_2.txt', '-o', 'out2_3.txt', '"echo text>out2_3.txt"'])
-    subprocess.call(['dvc', 'run', '--no-exec', '-d', 'out2_3.txt', '-o', 'out2_4_1.txt', '"echo text>out2_4_1.txt"'])
-    subprocess.call(['dvc', 'run', '-d', 'out2_3.txt', '-o', 'out2_4_2.txt', '"echo text>out2_4_2.txt"'])
-    subprocess.call(['dvc', 'run', '-d', 'out2_4_2.txt', '-o', 'out2_4_2_1.txt', '"echo text>out2_4_2_1.txt"'])
+    subprocess.call(['dvc', 'run', '-o', 'out2_1.txt', 'echo text>out2_1.txt'])
+    subprocess.call(['dvc', 'run', '-d', 'out2_1.txt', '-o', 'out2_2.txt', 'echo text>out2_2.txt'])
+    subprocess.call(['dvc', 'run', '-d', 'out2_2.txt', '-o', 'out2_3.txt', 'echo text>out2_3.txt'])
+    subprocess.call(['dvc', 'run', '--no-exec', '-d', 'out2_3.txt', '-o', 'out2_4_1.txt', 'echo text>out2_4_1.txt'])
+    subprocess.call(['dvc', 'run', '-d', 'out2_3.txt', '-o', 'out2_4_2.txt', 'echo text>out2_4_2.txt'])
+    subprocess.call(['dvc', 'run', '-d', 'out2_4_2.txt', '-o', 'out2_4_2_1.txt', 'echo text>out2_4_2_1.txt'])
     subprocess.call(
-        ['dvc', 'run', '--no-exec', '-d', 'out2_4_2.txt', '-o', 'out2_4_2_2.txt', '"echo text>out2_4_2_2.txt"'])
+        ['dvc', 'run', '--no-exec', '-d', 'out2_4_2.txt', '-o', 'out2_4_2_2.txt', 'echo text>out2_4_2_2.txt'])
     subprocess.call(
         ['dvc', 'run', '--no-exec', '-d', 'out2_4_2_2.txt', '-d', 'out2_4_2_1.txt', '-d', 'out2_4_1.txt', '-o',
-         'out2_5.txt', '"echo text>out2_5.txt"'])
+         'out2_5.txt', 'echo text>out2_5.txt'])
 
 def main():
     create_fix_demo_stages("TEST-FOLDER")
